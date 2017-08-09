576caaed7e91318c83eca273817d9951332dc30a
Mathias Mortari <mathias.mortari@gmail.com>
2017-08-08 21:28:56 -0300

    dialog for new objects

--- main.cpp
+++ main.cpp
@@ -11,6 +11,20 @@ std::list<DrawableObject> object_list;
 GtkBuilder *builder;
 GObject *window;
 GObject *drawing_area;
+GObject *new_obj_dialog;
+
+
+
+
+
+/*Function that will be called when the ok button is pressed*/
+ extern "C" G_MODULE_EXPORT void add_new_object_dialog()
+ {
+    //does not work
+  // gtk_window_present( GTK_WINDOW( new_obj_dialog ) );
+ } 
+
+
 
 int main (int   argc, char *argv[])
 {
@@ -25,6 +39,7 @@ int main (int   argc, char *argv[])
   g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
 
   drawing_area = gtk_builder_get_object(builder, "drawing_area");
+  new_obj_dialog = gtk_builder_get_object(builder, "window_add_new_object");
 
   gtk_main ();
 
