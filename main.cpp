fa4ca130cbd8b49aad4f66f5d7cad9bfee444af9
Mathias Mortari <mathias.mortari@gmail.com>
2017-08-10 10:48:19 -0300

    adjustments on popup, which is still not working

--- main.cpp
+++ main.cpp
@@ -9,20 +9,17 @@
 std::list<DrawableObject> object_list;
 
 GtkBuilder *builder;
-GObject *window;
-GObject *drawing_area;
-GObject *new_obj_dialog;
-
-
+GtkWidget *window;
+GtkWidget *drawing_area;
+GtkWidget *new_object_dialog;
 
 
 
 /*Function that will be called when the ok button is pressed*/
- extern "C" G_MODULE_EXPORT void add_new_object_dialog()
- {
-    //does not work
-  // gtk_window_present( GTK_WINDOW( new_obj_dialog ) );
- } 
+G_MODULE_EXPORT void add_new_object_dialog()
+{
+  gtk_window_present( GTK_WINDOW( new_object_dialog ) );
+} 
 
 
 
@@ -35,11 +32,14 @@ int main (int   argc, char *argv[])
   gtk_builder_add_from_file (builder, "window.glade", NULL);
 
   /* Connect signal handlers to the constructed widgets. */
-  window = gtk_builder_get_object (builder, "window");
-  g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+  window = GTK_WIDGET( gtk_builder_get_object( builder, "window" ) );
+  //drawing_area = GTK_WIDGET( gtk_builder_get_object( builder, "drawing_area" ) );
+  new_object_dialog = GTK_WIDGET( gtk_builder_get_object( builder, "new_object_dialog" ) );
 
-  drawing_area = gtk_builder_get_object(builder, "drawing_area");
-  new_obj_dialog = gtk_builder_get_object(builder, "window_add_new_object");
+
+  g_signal_connect (window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+  g_signal_connect (window, "configure-event", G_CALLBACK (add_new_object_dialog), NULL);
+  //g_signal_connect (drawing_area, "draw", G_CALLBACK (redraw), NULL);
 
   gtk_main ();
 
