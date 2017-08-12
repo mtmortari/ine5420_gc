99e36c83a336a683b3e863a89bdead7257a4c1de
Mathias Mortari <mathias.mortari@gmail.com>
2017-08-12 09:56:51 -0300

    transform functions in view class

--- View.cpp
+++ View.cpp
@@ -1,42 +1,51 @@
 #include "View.h"
 
+double View::transformX(double x_w, View window)
+{
+	return ((x_w - window.getXMin()) / ( window.getXMax() - window.getXMin() ))*(this->x_max - this->x_min);
+}
 
-void View::setXMin(int x_min)
+double View::transformY(double y_w, View window)
+{
+	return ( 1 - ( (y_w - window.getYMin()) / ( window.getYMax() - window.getYMin()) ) )*(this->y_max - this->y_min);
+}
+
+void View::setXMin(double x_min)
 {
 	this->x_min = x_min;
 }
 
-void View::setYMin(int y_min)
+void View::setYMin(double y_min)
 {
 	this->y_min = y_min;
 }
 
-void View::setXMax(int x_max)
+void View::setXMax(double x_max)
 {
 	this->x_max = x_max;
 }
 
-void View::setYMax(int y_max)
+void View::setYMax(double y_max)
 {
 	this->y_max = y_max;
 }
 
-int View::getXMin()
+double View::getXMin()
 {
 	return x_min;
 }
 
-int View::getXMax()
+double View::getXMax()
 {
 	return x_max;
 }
 
-int View::getYMin()
+double View::getYMin()
 {
 	return y_min;
 }
 
-int View::getYMax()
+double View::getYMax()
 {
 	return y_max;
 }
--- View.h
+++ View.h
@@ -5,19 +5,22 @@
 #ifndef VIEW_H
 #define VIEW_H
 
-class VIEW{
+class View{
 	private:
-		int x_min, y_min, x_max, y_max;
+		double x_min, y_min, x_max, y_max;
 
 	public:		
-		void setXMin(int x_min);
-		void setYMin(int y_min);
-		void setXMax(int x_max);
-		void setYMax(int y_max);
-		int getXMin();
-		int getXMax();
-		int getYMin();
-		int getYMax();	
+		void setXMin(double x_min);
+		void setYMin(double y_min);
+		void setXMax(double x_max);
+		void setYMax(double y_max);
+		double getXMin();
+		double getXMax();
+		double getYMin();
+		double getYMax();	
+
+		double transformX(double x_w, View window);
+		double transformY(double y_w, View window);
 };
 
 #endif
